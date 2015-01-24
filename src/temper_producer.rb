c8adc8ec1279a04da3a37f7f0d8fb5590c9465cd
Yuya Fujiwara <yuya@Yuya-MacBook-Air.local>
2015-01-24 22:25:47 +0900

    modfiy temper_producer.rb

--- src/temper_producer.rb
+++ src/temper_producer.rb
@@ -2,19 +2,30 @@
 # -*- encoding: utf-8 -*-
 require 'poseidon'
 
+# 温度をKafkaにProduceするClass
 class TemperProducer
 	@producer = nil
 	@topic = nil
-	def initialize(broker_host, client_id, topic)
-    puts topic
+	
+  def initialize(broker_host, client_id, topic)
 		@topic = topic
-    puts @topic
-    puts @client_id
-    puts @broker_host
-		@producer = Producer.new(broker_host, client_id, :type => :sync, :compression_codec => :gzip)
+		@producer = Producer.new(get_broker_list, client_id, :type => :sync, :compression_codec => :gzip)
 	end
 
+  private 
+  def get_broker_list(broker_host)
+    broker_list = []
+    broker_host = broker_host.gsub(" ", "")
+    if broker_host.count(",") > 0
+      broker_list = broker_host.split(",")
+    else
+      broker_list << broker_host
+    end
+  end
+
 	def send(message)
-		@producer.send_messages([Poseidon::MessageToSend.new(@topic, message)])
+    message = []
+    message << Poseidon::MessageToSend.new(@topic, message)
+		@producer.send_messages(meesage)
 	end
 end
